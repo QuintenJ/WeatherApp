db3b2a64768813ca6412e35872805b44f08019ac
Quinten <me@quintenjames.com>
2021-08-21 10:53:30 -0600

    Create OpenWeatherMap Models
    Models for JSON API Respone

---
+++ OpenWeatherModels/Coord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+
+
+namespace WeatherApp.OpenWeatherModels
+{
+    public class Coord
+    {
+        public float Lon { get; set; }
+        public float Lat { get; set; }
+    }
+}
