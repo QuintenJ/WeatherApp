3ebaf9eddf501f58df83377241ed1b314e3e79a5
Quinten <me@quintenjames.com>
2021-08-21 01:18:55 -0600

    Create Controller
    GET: WeatherApp/SearchCity POST:WeatherApp/SearchCity GET: WeatherApp/City setup

---
+++ Controllers/WeatherAppController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApp.WeatherAppModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherAppController : Controller
+    {
+        // GET: WeatherApp/SearchCity
+        public IActionResult SearchCity()
+        {
+            var viewModel = new SearchCity();
+            return View(viewModel);
+        }
+
+        // POST: WeatherApp/SearchCity
+        [HttpPost]
+        public IActionResult SearchCity(SearchCity model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherApp", new { city = model.CityName });
+            }
+            return View(model);
+        }
+
+        // GET: WeatherApp/City
+        public IActionResult City(string city)
+        {
+            City viewModel = new City();
+            return View(viewModel);
+        }
+    }
+}
